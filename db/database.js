1a6536aa449bd4fba2ca0c1986d0fe51a832ef0e
Jean-Kerby <jeankerby4@gmail.com>
2023-08-30 23:24:10 -0400

    remove passowrd to Mysql

--- db/database.js
+++ db/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'MyBar2',
-    password: '105Kerby@sql'
+    password: ''
 });
 
 module.exports = pool.promise();
